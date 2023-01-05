7b35e5c1865c5640430cb8c780cdd4a60de2ca4c
jannikolai.rueckert <jan.nr@live.de>
2023-01-05 10:27:23 +0100

    Fetch team data from API (jsonplaceholder)

--- corporate-website-react/src/components/Team/Team.js
+++ corporate-website-react/src/components/Team/Team.js
@@ -1,8 +1,29 @@
+import {useEffect, useState} from "react";
+
 const Team = () => {
+
+    const [users, setUsers] = useState(null)
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(res => {
+                return res.json()
+            })
+            .then(data => {
+                setUsers(data)
+            })
+    }, []);
+
     return (
         <section className="page-content">
             <div className="container">
-                <h1>Team</h1>
+                <h2>Our team</h2>
+                { users && users.map(a => (
+                        <div className="card" key={a.id}>
+                            <h3>{a.name}</h3>
+                            <p>{a.email}</p>
+                        </div>
+                    ))}
             </div>
         </section>
     );
