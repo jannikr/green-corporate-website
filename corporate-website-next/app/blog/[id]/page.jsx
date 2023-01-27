7f07bb1a7bb520e3f02b6b1f4531cc7c924239b3
jannikolai.rueckert <jan.nr@live.de>
2023-01-27 17:19:16 +0100

    Add generateStaticParams in Article component in order to render articles statically

--- corporate-website-next/app/blog/[id]/page.jsx
+++ corporate-website-next/app/blog/[id]/page.jsx
@@ -1,27 +1,42 @@
 import Articles from '../../../data/articles.json'
-function getArticle(articleId){
+
+export function getArticle(articleId) {
     const article = Articles.find(el => el.id === articleId)
     return article
 }
 
-const Article = ({params}) => {
+export async function generateStaticParams() {
+    return Articles.map(article => ({
+        id: article.id
+    }))
+}
 
-    const article = getArticle(params.id)
-    console.log("Hello")
+const Article = async ({params}) => {
 
-    return(
-        <section className="page-content">
-            <div className="container">
-                <div className="card">
-                    <h3>{article.title}</h3>
-{/*                    TODO: Add Image to article
+    const article = await getArticle(params.id)
+
+    if (article) {
+        return (
+            <section className="page-content">
+                <div className="container">
+                    <div className="card">
+                        <h3>{article.title}</h3>
+                        {/*                    TODO: Add Image to article
                     <img src={require("../../assets/blog/" + image)} style={imageStyle} alt=""/>*/}
-                    <p>{article.body}</p>
+                        <p>{article.body}</p>
+                    </div>
                 </div>
-            </div>
-        </section>
-    )
-
+            </section>
+        )
+    } else {
+        return (
+            <section className="page-content">
+                <div className="container">
+                     <p>Page not found.</p>
+                </div>
+            </section>
+        )
+    }
 }
 
 export default Article
