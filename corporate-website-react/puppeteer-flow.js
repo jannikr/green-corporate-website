2abb043cf4367ceb86d54fad4467c08119ee0424
jannikolai.rueckert <jan.nr@live.de>
2023-02-15 16:30:26 +0100

    Add config files for measurement

---
+++ corporate-website-react/puppeteer-flow.js
@@ -0,0 +1,32 @@
+const puppeteer = require("puppeteer");
+const microtime = require("microtime");
+
+(async () => {
+    console.log(microtime.now()," Launching browser");
+    const browser = await puppeteer.launch({
+        defaultViewport: {
+            width: 1920,
+            height: 1080,
+        },
+        headless: true,
+        executablePath: "/usr/bin/chromium-browser",
+        args: ["--no-sandbox", "--disable-setuid-sandbox"], // Otherwise it won't run on Docker
+    });
+
+    const page = await browser.newPage();
+    const dimensions = await page.evaluate(() => {
+        return {
+            width: document.documentElement.clientWidth,
+            height: document.documentElement.clientHeight,
+            deviceScaleFactor: window.devicePixelRatio,
+        };
+    });
+
+    console.log(microtime.now()," Home Page");
+    await page.goto("http://corporate-website-react-server:8080", {
+        waitUntil: "networkidle2",
+    });
+
+    console.log(microtime.now()," Closing browser");
+    await browser.close();
+})();
