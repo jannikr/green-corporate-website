3b6d82b08765b3eb9ce6f8255ea4cd125a50e95b
jannikolai.rueckert <jan.nr@live.de>
2023-04-26 13:06:34 +0200

    Change usage scenarios

--- corporate-website-next-v2/puppeteer-flow.js
+++ corporate-website-next-v2/puppeteer-flow.js
@@ -29,25 +29,23 @@ const microtime = require("microtime");
         waitUntil: "networkidle2",
     });
 
-    // Todo: Scrollen...
-
     console.log(microtime.now()," About Page");
-    await page.click('[id="about-navbar-link"]');
+    await page.click('[id="about-navbar-link"]', {delay: 4000});
     await page.waitForSelector("#about-title");
 
     console.log(microtime.now()," Team Page");
-    await page.click('[id="team-footer-link"]');
+    await page.click('[id="team-footer-link"]', {delay: 4000});
     await page.waitForSelector("#team-title");
 
     console.log(microtime.now()," Blog Page");
-    await page.click('[id="blog-navbar-link"]');
+    await page.click('[id="blog-navbar-link"]', {delay: 4000});
     await page.waitForXPath('//h1[contains(text(), "Next Blog")]')
 
-    // Todo: Scrollen...
-
     console.log(microtime.now()," Article 3");
-    await page.click('[id="blog-id-3"]');
+    await page.click('[id="blog-id-3"]', {delay: 4000});
     await page.waitForXPath('//h3[contains(text(), "At vero eos")]')
+
+    await new Promise(r => setTimeout(r, 4000));
 
     console.log(microtime.now()," Closing browser");
     await browser.close();
