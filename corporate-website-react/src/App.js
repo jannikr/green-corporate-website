545fa433498c7cc0a5fd823ee6f48625296045b1
jannikolai.rueckert <jan.nr@live.de>
2023-05-16 13:23:23 +0200

    Add code splitting with lazy loading

--- corporate-website-react/src/App.js
+++ corporate-website-react/src/App.js
@@ -2,25 +2,30 @@ import './App.css';
 import Navbar from "./components/Layout/Navbar";
 import Footer from "./components/Layout/Footer";
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import Home from "./components/Home/Home";
-import About from "./components/About/About";
-import Team from "./components/Team/Team";
-import Blog from "./components/Blog/Blog";
-import Article from "./components/Blog/Article";
+import { lazy, Suspense } from "react";
+
+// Dynamic import
+const Home = lazy(() =>import('./components/Home/Home'));
+const About = lazy(() =>import('./components/About/About'));
+const Team = lazy(() =>import('./components/Team/Team'));
+const Blog = lazy(() =>import('./components/Blog/Blog'));
+const Article = lazy(() =>import('./components/Blog/Article'));
 
 function App() {
 
     return (
         <Router>
             <Navbar/>
-            <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="/about" element={<About/>}/>
-                <Route path="/team" element={<Team/>}/>
-                <Route path="/blog" element={<Blog/>}/>
-                <Route path="/blog/:id" element={<Article/>}/>
-            </Routes>
-            <Footer />
+            <Suspense fallback={<p>Loading...</p>}>
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/about" element={<About/>}/>
+                    <Route path="/team" element={<Team/>}/>
+                    <Route path="/blog" element={<Blog/>}/>
+                    <Route path="/blog/:id" element={<Article/>}/>
+                </Routes>
+            </Suspense>
+            <Footer/>
         </Router>
     );
 }
